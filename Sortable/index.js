5fd4354b6caa5f17b9469d69433d9069aad0010d
Jaume Fornes Flexas <jaume.fornes@etoolinnovation.com>
2023-06-01 10:46:26 +0200

    fix

--- Sortable/index.js
+++ Sortable/index.js
@@ -1,7 +1,3 @@
-export {default as SortableContainer} from './SortableContainer';
-export {default as SortableElement} from './SortableElement';
-export {default as SortableHandle} from './SortableHandle';
-
 export {default as sortableContainer} from './SortableContainer';
 export {default as sortableElement} from './SortableElement';
 export {default as sortableHandle} from './SortableHandle';
